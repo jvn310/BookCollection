1e3b11fd15e199325e8ebe95bdca7c0fc0601164
tumelo <132863300+jvn310@users.noreply.github.com>
2024-11-29 18:41:54 +0200

    added books controller

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookCollection.Data;
+using BookCollection.Models;
+using System.Linq;
+
+namespace BookCollection.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Index: Display all books
+        public IActionResult Index()
+        {
+            var books = _context.Books.ToList();
+            return View(books);
+        }
+
+        // Add Book (GET)
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // Add Book (POST)
+        [HttpPost]
+        public IActionResult Add(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+        // Edit Book (GET)
+        public IActionResult Edit(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book == null) return NotFound();
+            return View(book);
+        }
+
+        // Edit Book (POST)
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Books.Update(book);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+        // Delete Book
+        public IActionResult Delete(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book == null) return NotFound();
+
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
