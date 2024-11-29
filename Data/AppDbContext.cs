831a91356c2fff2eded64cc2b21fbc84eb6cfec5
tumelo <132863300+jvn310@users.noreply.github.com>
2024-11-29 18:37:52 +0200

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookCollection.Models;
+
+namespace BookCollection.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "Server=localhost;Database=BookDb;User=root;Password=67479@Jade10;",
+                new MySqlServerVersion(new Version(8, 0, 40))
+            );
+        }
+    }
+}
